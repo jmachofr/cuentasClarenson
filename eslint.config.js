1baa857a2ee99e1bdbf5f327a1483768ef8672a0
Julian Macho Fraga <machofraga.julian@hotmail.com>
2025-10-20 19:32:38 -0300

    app creation

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser }, rules: {"react/react-in-jsx-scope": "off"} }
+]);
