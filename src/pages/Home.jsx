60aed335d633b1a2d684ab34eebef96647053981
Julian Macho Fraga <machofraga.julian@hotmail.com>
2025-11-13 23:14:41 -0300

    home page theme and styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
+import logo from '../images/logo-main.png';
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
     <div className="home-container">
-      <h1>Home Menu</h1>
+      <div className="home-header">
+        <div className="home-logo">
+          <img src={logo} alt="Home Logo" />
+        </div>
+        <div className='home-title'>
+          <span>Cuentas</span>
+          <span>Clarenson</span>
+        </div>
+      </div>
       <div className="home-buttons">
         <button onClick={() => navigate('/new-expense')}>New Expense</button>
         <button onClick={() => navigate('/expense-history')}>Previous Expenses</button>
